d653361300e2256bb549dec47002afbdc6416a27
fadhilsaheer <fadhilsaheer@gmail.com>
2021-08-19 10:14:05 +0530

    fized freaking setup bug

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({Key? key}) : super(key: key);
@@ -12,6 +13,17 @@ class ChatScreen extends StatelessWidget {
           padding: EdgeInsets.all(8),
           child: Text('This Works'),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          FirebaseFirestore.instance
+              .collection('chats/wHgoeKvvYMuigvuyc8jF/messages')
+              .snapshots()
+              .listen((event) {
+            print(event);
+          });
+        },
       ),
     );
   }
