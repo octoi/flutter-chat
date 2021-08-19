372ffb81fe4bf199150ded36875cf9eeb55f94fc
fadhilsaheer <fadhilsaheer@gmail.com>
2021-08-19 12:34:18 +0530

    message listener

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutterchat/widgets/chats/messages.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({Key? key}) : super(key: key);
@@ -38,33 +39,16 @@ class ChatScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: StreamBuilder(
-        stream: FirebaseFirestore.instance
-            .collection('chats/TzPr57aL5F0f62aB6D2A/messages')
-            .snapshots(),
-        builder: (ctx, streamSnapshot) {
-          if (streamSnapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          }
-
-          dynamic data = streamSnapshot.data;
-          dynamic documents = data.docs;
-          return ListView.builder(
-            itemCount: documents!.length,
-            itemBuilder: (ctx, index) => Container(
-              padding: EdgeInsets.all(8),
-              child: Text(documents[index]['text']),
-            ),
-          );
-        },
+      body: Container(
+        child: Column(
+          children: [
+            Expanded(child: Messages()),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {
-          FirebaseFirestore.instance
-              .collection('chats/TzPr57aL5F0f62aB6D2A/messages')
-              .add({'text': 'hey its ${DateTime.now()}'});
-        },
+        onPressed: () {},
       ),
     );
   }
